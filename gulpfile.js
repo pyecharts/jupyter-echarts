23fb1987d31e684c6d3cb0942d767e70bee5570c
chfw <wangc_2011@hotmail.com>
2017-08-14 23:51:20 +0100

    add gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require("gulp");
+var ts = require("gulp-typescript");
+var tsProject = ts.createProject("tsconfig.json");
+var minify = require("gulp-minify");
+var rename = require('gulp-rename');
+
+
+FILES = [
+	'./node_modules/echarts/dist/echarts.min.js',
+	'./node_modules/echarts/map/js/china.js',
+	'./node_modules/echarts/map/js/province/*.js',
+	'./node_modules/echarts-gl/dist/echarts-gl.min.js',
+	'./node_modules/echarts-liquidfill/dist/echarts-liquidfill.min.js',
+	'./node_modules/echarts-wordcloud/dist/echarts-wordcloud.min.js'
+	]
+
+gulp.task("default", function () {
+	tsProject.src()
+        .pipe(tsProject())
+        .js.pipe(gulp.dest("dist"));
+	gulp.src(['dist/main.js'])
+		.pipe(minify({
+			ext: { src: ".js"}
+		}))
+		.pipe(gulp.dest('echarts'));
+	return gulp.src(FILES, {base: './node_modules'})
+		.pipe(rename({dirname: ''}))
+		.pipe(gulp.dest('echarts'));
+});
