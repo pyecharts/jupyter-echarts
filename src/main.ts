5f60cf602e3205292259e6a5cf82601030230708
chfw <wangc_2011@hotmail.com>
2017-08-27 13:50:05 +0100

    :lipstick: clearly show which version was loaded in console

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+var version = '1.0.2';
+
 export function load_ipython_extension(){
-   console.log("jupyter-echarts has been loaded");
+   console.log("jupyter-echarts " + version + " (echarts 3.6.2) has been loaded");
 }
