a58ec45e1f27717d582e083a42f76d96e0868a96
chfw <wangc_2011@hotmail.com>
2017-12-21 19:21:42 +0000

    :books: update jupyter module loaded message

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
-var version = '1.3.3';
+var version = '1.4.0';
 
 export function load_ipython_extension() {
-    console.log("jupyter-echarts " + version + " (echarts 3.6.2) has been loaded");
+    console.log("jupyter-echarts " + version + " (echarts 3.8.5) has been loaded");
 }
