387ad2b22f9c5c0e880659d8dfd14eb20aba6768
chfw <wangc_2011@hotmail.com>
2017-12-27 19:37:21 +0000

    :hammer: separate the taiwan build steps in two as the UTF8-encoded geojson will be fed into echarts project

--- build/tasks/taiwan.js
+++ build/tasks/taiwan.js
@@ -7,7 +7,8 @@ var minify = require("gulp-minify");
 gulp.task("taiwan", function(){
   maker.merge('taiwan/taiwan.json',
               'taiwan/diaoyudao.json');
-  maker.makeJs('merged_taiwan.json', './dist/taiwan.js', '台湾');
+  maker.compress('merged_taiwan.json', 'merged_and_compressed_taiwan.json');
+  maker.makeJs('merged_and_compressed_taiwan.json', './dist/taiwan.js', '台湾');
   gulp.src('./dist/taiwan.js', {base: './dist'})
 	.pipe(minify({
       noSource: true,
