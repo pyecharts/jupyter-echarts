8eabdb33b20a3a292f72fbccff07328239a45e04
chfw <wangc_2011@hotmail.com>
2018-01-18 18:06:46 +0000

    :art: minor update

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 var version = '1.4.0';
+var echarts = '4.0.2';
 
 export function load_ipython_extension() {
-    console.log("jupyter-echarts " + version + " (echarts 3.8.5) has been loaded");
+    console.log(`jupyter-echarts ${version} (echarts ${echarts}) has been loaded`);
 }
